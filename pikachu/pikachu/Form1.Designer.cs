715144d34d5f2f2509995e9e1f8eb1f41e94aa51
John6tain4eto <john6tain.tube@gmail.com>
2015-09-30 17:09:58 +0300

    this is animation and wallking almost

--- pikachu/pikachu/Form1.Designer.cs
+++ pikachu/pikachu/Form1.Designer.cs
@@ -32,8 +32,10 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox2
@@ -56,15 +58,25 @@
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
             this.timer1.Interval = 400;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::pikachu.Properties.Resources._7;
+            this.pictureBox3.Location = new System.Drawing.Point(331, 158);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 164);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 374);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -72,6 +84,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,6 +94,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- pikachu/pikachu/Form1.cs
+++ pikachu/pikachu/Form1.cs
@@ -20,16 +20,90 @@ namespace pikachu
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            timer1.Enabled = false;
 
-         /*  pictureBox1.Visible = true;
-            pictureBox2.Visible = false;*/
-
-            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             pictureBox2.Visible = pictureBox2.Visible == true ? false : true;
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs ev)
+        {
+
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+            int x1 = pictureBox2.Location.X;
+            int y1 = pictureBox2.Location.Y;
+
+            if (ev.KeyCode == Keys.Right)
+            {
+
+                x += 5;
+                x1 += 5;
+                pictureBox1.Location = new Point(x, y);
+                pictureBox2.Location = new Point(x, y);
+                timer1.Enabled = true;
+              //  
+                if (pictureBox1.Bounds.IntersectsWith(pictureBox3.Bounds))
+                {
+                    x -= 5;
+                    x1 -= 5;
+                    timer1.Enabled = false;
+                }
+                pictureBox1.Location = new Point(x, y);
+                pictureBox2.Location = new Point(x, y);
+               // timer1.Enabled = false;
+
+            }
+            else if (ev.KeyCode == Keys.Left)
+            {
+
+                x -= 5;
+                x1 -= 5;
+                pictureBox1.Location = new Point(x, y);
+                pictureBox2.Location = new Point(x, y);
+                if (pictureBox1.Bounds.IntersectsWith(pictureBox3.Bounds))
+                {
+                    x += 5;
+                    x1 += 5;
+                }
+                pictureBox1.Location = new Point(x, y);
+                pictureBox2.Location = new Point(x, y);
+            }
+            else if (ev.KeyCode == Keys.Up)
+            {
+                y -= 5;
+                y1 -= 5;
+                pictureBox1.Location = new Point(x, y);
+                pictureBox2.Location = new Point(x, y);
+                if (pictureBox1.Bounds.IntersectsWith(pictureBox3.Bounds))
+                {
+                    y += 5;
+                    y1 += 5;
+                }
+                pictureBox1.Location = new Point(x, y);
+                pictureBox2.Location = new Point(x, y);
+            }
+
+            else if (ev.KeyCode == Keys.Down)
+            {
+                y += 5;
+                y1 += 5;
+                pictureBox1.Location = new Point(x, y);
+                pictureBox2.Location = new Point(x, y);
+                if (pictureBox1.Bounds.IntersectsWith(pictureBox3.Bounds))
+                {
+                    y -= 5;
+                    y1 -= 5;
+                }
+                pictureBox1.Location = new Point(x, y);
+                pictureBox2.Location = new Point(x, y);
+            }
+            
+
 
         }
 
